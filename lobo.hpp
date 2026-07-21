b4c93c45bb22d23bc414adbda705b4adcb85a5d3
João Vitor <joaovitoralvesmartins10@gmail.com>
2026-07-21 17:09:59 -0300

    Adição: lobo

---
+++ lobo.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "personagem.hpp"
+using namespace std;
+
+class Lobo : Personagem {
+
+    public:
+
+    Lobo() : Personagem(
+        "Lobo",
+        10,
+        5,
+        5,
+        3,
+        1,
+        2,
+        10)
+    {
+    }
+};
