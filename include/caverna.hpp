c2b2f2a89fd50fdfd18280f4dc28b3609e4f7b7d
João Vitor <joaovitoralvesmartins10@gmail.com>
2026-07-23 16:52:07 -0300

    Adição de caverna

---
+++ include/caverna.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "personagem.hpp"
+
+class Caverna {
+
+    public:
+
+        void Explorar(Personagem& jogador);
+};
---
+++ src/caverna.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "caverna.hpp"
+using namespace std;
+
+void Caverna::Explorar(Personagem& jogador){
+
+    cout << jogador.nome << " entrou na caverna" << endl;
+    cout << "Um morcego apareceu!" << endl;
+    cout << endl;
+}
